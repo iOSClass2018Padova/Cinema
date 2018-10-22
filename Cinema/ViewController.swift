932a6ffd5286d611d7ca147722f60d026ef41d1a
Stefano Vecchiati <stefano.vecchiati@aol.com>
2018-10-22 10:00:44 +0200

    Initial Commit

---
+++ Cinema/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Cinema
+//
+//  Created by stefano vecchiati on 22/10/2018.
+//  Copyright © 2018 com.stefanovecchiati. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
